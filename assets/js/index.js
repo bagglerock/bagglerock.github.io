a987b212e65c27c80086531c2a24589eb073f1c9
Oscar Villalta <bagglerock@gmail.com>
2018-05-30 14:52:08 -0400

    fixing some formatting issues

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+$(window).scroll(function(){
+    $(".hero-left").css("opacity", 1 - $(window).scrollTop() / 250);
+    $("#quote").css("opacity", 1 - $(window).scrollTop() / 700);
+    //$(".websites-header").css("opacity", 0 + $(window).scrollTop() / 500);
+    //$(".websites-header").css("opacity", 1 + $(window).scrollTop() / 1200);
+  });
