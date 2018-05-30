727fcbb0b67faeb0f76c6398cfb425e356828ef8
Oscar Villalta <bagglerock@gmail.com>
2018-05-30 19:45:51 -0400

    changed the footer to just a pic

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 $(window).scroll(function(){
     $(".hero-left").css("opacity", 1 - $(window).scrollTop() / 250);
     $(".right-content").css("opacity", 1 - $(window).scrollTop() / 700);
-    //$(".websites-header").css("opacity", 0 + $(window).scrollTop() / 500);
+    $(".header").css("opacity", 0 + $(window).height() / 500);
     //$(".websites-header").css("opacity", 1 + $(window).scrollTop() / 1200);
   });
